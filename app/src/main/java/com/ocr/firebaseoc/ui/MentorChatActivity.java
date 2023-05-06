87cab181d4500fa7ce4e9813a7c933a031056f94
abliveira <dev.ab@outlook.com>
2023-05-06 01:10:40 -0300

    Image selection

--- app/src/main/java/com/ocr/firebaseoc/ui/MentorChatActivity.java
+++ app/src/main/java/com/ocr/firebaseoc/ui/MentorChatActivity.java
@@ -1,26 +1,41 @@
 package com.ocr.firebaseoc.ui;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.Query;
+import com.ocr.firebaseoc.R;
 import com.ocr.firebaseoc.databinding.ActivityMentorChatBinding;
 import com.ocr.firebaseoc.manager.ChatManager;
 import com.ocr.firebaseoc.manager.UserManager;
 import com.ocr.firebaseoc.model.Message;
 import com.ocr.firebaseoc.ui.chat.MentorChatAdapter;
 
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
 public class MentorChatActivity extends BaseActivity<ActivityMentorChatBinding> implements MentorChatAdapter.Listener {
 
     private MentorChatAdapter mentorChatAdapter;
     private String currentChatName;
+
+    private static final String PERMS = Manifest.permission.READ_EXTERNAL_STORAGE;
+    private static final int RC_IMAGE_PERMS = 100;
+    private static final int RC_CHOOSE_PHOTO = 200;
 
     private static final String CHAT_NAME_ANDROID = "android";
     private static final String CHAT_NAME_BUG = "bug";
@@ -28,6 +43,8 @@ public class MentorChatActivity extends BaseActivity<ActivityMentorChatBinding> 
 
     private UserManager userManager = UserManager.getInstance();
     private ChatManager chatManager = ChatManager.getInstance();
+
+    private Uri uriImageSelected;
 
     @Override
     protected ActivityMentorChatBinding getViewBinding() {
@@ -41,15 +58,53 @@ public class MentorChatActivity extends BaseActivity<ActivityMentorChatBinding> 
         setupListeners();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
     private void setupListeners(){
 
         // Send button
         binding.sendButton.setOnClickListener(view -> { sendMessage(); });
+        binding.addFileButton.setOnClickListener(view -> { addFile(); });
 
         // Chat buttons
         binding.androidChatButton.setOnClickListener(view -> { this.configureRecyclerView(CHAT_NAME_ANDROID); });
         binding.firebaseChatButton.setOnClickListener(view -> { this.configureRecyclerView(CHAT_NAME_FIREBASE); });
         binding.bugChatButton.setOnClickListener(view -> { this.configureRecyclerView(CHAT_NAME_BUG); });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponse(requestCode, resultCode, data);
+    }
+
+    @AfterPermissionGranted(RC_IMAGE_PERMS)
+    private void addFile(){
+        if (!EasyPermissions.hasPermissions(this, PERMS)) {
+            EasyPermissions.requestPermissions(this, getString(R.string.popup_title_permission_files_access), RC_IMAGE_PERMS, PERMS);
+            return;
+        }
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, RC_CHOOSE_PHOTO);
+    }
+
+    // Handle activity response (after user has chosen or not a picture)
+    private void handleResponse(int requestCode, int resultCode, Intent data){
+        if (requestCode == RC_CHOOSE_PHOTO) {
+            if (resultCode == RESULT_OK) { //SUCCESS
+                this.uriImageSelected = data.getData();
+                Glide.with(this) //SHOWING PREVIEW OF IMAGE
+                        .load(this.uriImageSelected)
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(binding.imagePreview);
+            } else {
+                Toast.makeText(this, getString(R.string.toast_title_no_image_chosen), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     // Configure RecyclerView
