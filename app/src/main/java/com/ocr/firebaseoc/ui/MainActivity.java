5ff098cbb398b350776dd3632583c572373dad30
abliveira <dev.ab@outlook.com>
2023-05-06 13:36:55 -0300

    Crashlytics setup

--- app/src/main/java/com/ocr/firebaseoc/ui/MainActivity.java
+++ app/src/main/java/com/ocr/firebaseoc/ui/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setupListeners();
+        throw new RuntimeException("Test Crash"); // Keep commented. Generates a test crash
     }
 
     @Override
